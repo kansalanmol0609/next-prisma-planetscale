30799c63fe93f4832a7b6678051400716bcbcd1f
Anmol Kansal <kansalanmol0609@gmail.com>
2022-11-19 18:28:48 +0530

    Adds Database

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,59 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 
 type Data = {
-  name: string
+	message?: string;
+	error?: string;
+	success: boolean;
+};
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
+) {
+	if (req.method === "POST") {
+		return await addBook(req, res);
+	} else if (req.method === "GET") {
+		return await readBooks(req, res);
+	} else {
+		return res
+			.status(405)
+			.json({ message: "Method not allowed", success: false });
+	}
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+async function readBooks(req, res) {
+	const body = req.body;
+
+	try {
+		const allBooks = await prisma.bookSuggestion.findMany();
+
+		return res.status(200).json(allBooks, { success: true });
+	} catch (error) {
+		console.log(error);
+
+		return res
+			.status(500)
+			.json({ error: "Error reading from database", success: false });
+	}
+}
+
+async function addBook(req: NextApiRequest, res: NextApiResponse<Data>) {
+	const body = req.body;
+	try {
+		const newEntry = await prisma.bookSuggestion.create({
+			data: {
+				bookTitle: body.title,
+				bookAuthor: body.author,
+				bookGenre: body.genre,
+			},
+		});
+		return res.status(200).json(newEntry, { success: true });
+	} catch (error) {
+		console.error("Request error", error);
+		res.status(500).json({ error: "Error adding book", success: false });
+	}
 }
